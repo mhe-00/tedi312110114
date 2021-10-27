bbf658c982d8a1a7e1e29f4889ab338a6c6a96f3
Tedi Mulyadi <tedimhe00@pelitabangsa.ac.id>
2021-10-27 17:49:12 +0700

    menambahkan folder biodata dan latihan1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tedi312110114
+
+## latihan1
+![gambar](screeshot1/gambar1.png)
---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("hello")
+print("saya sedang belajar python")
+
+a = 8
+b = 6
+print ("variable a=" ,a)
+print ("variable b=" ,b)
+print ("hasil penjumlahan a+b=", a+b)
+
+a=input("masukan nilai a:")
+b=input("masukan nilai b:")
+print("variable a=" ,a)
+print("variable b=" ,b)
+print("hasil penggabungan {0} & {1} = %s" .format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {0} + {1} = %d" .format(a,b) %(a+b))
+print("hasil pembagian {0} / {1} = %f" .format(a,b) %(a/b))
